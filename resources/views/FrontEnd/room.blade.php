59cdd024b5ee2ea10738d509ac5b15759b21395a
Lich2013 <longzy2013@gmail.com>
2016-05-25 15:19:57 +0800

    room 大概ok?

--- resources/views/FrontEnd/room.blade.php
+++ resources/views/FrontEnd/room.blade.php
@@ -1,2 +1,34 @@
 @extends('layouts.layout')
 @section('title', $room['user']['nickname'].'的直播间')
+@section('style')
+    <link rel="stylesheet" href="{{asset('css/videojs.css')}}">
+@endsection
+
+@section('script')
+    <script src="{{asset('js/video.js')}}"></script>
+    <script src="{{asset('js/videojs-contrib-hls.min.js')}}"></script>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1>{{$room['user']['nickname'].'的直播间'}}</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center center-block">
+            <video id="live-video" width="1280" height="720" class="video-js vjs-default-skin center-block" controls>
+                <source src="/live/live_room_id_{{$room['id']}}.m3u8" type="application/x-mpegURL">
+            </video>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            我是评论区
+        </div>
+    </div>
+    <script>
+        var player = videojs('live-video');
+        player.play();
+    </script>
+@endsection
